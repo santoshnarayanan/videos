0a24b42dce2386aa6225c01f657d929042d38b1b
santosh33 <santosh33@gmail.com>
2022-06-18 23:59:16 +0530

    fetch youtube api data in searchBar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import SearchBar from "./searchBar";
+import youtube from "./apis/youtube";
 import React from "react";
 
 class App extends React.Component{
+
+    state = {videos: [] };
+
+    onTermSubmit = async (term) =>{
+        const response = await youtube.get('/search', {
+            params:{
+                q:term
+            }
+        });
+        this.setState({videos: response.data.items});
+    };
+
     render(){
         return (
-        <div>
-            <SearchBar />
-        </div>
+            <div className="ui container">
+                <SearchBar onFormSubmit ={this.onTermSubmit} />
+                I have {this.state.videos.length} videos.
+            </div>
         );
     }
 }
