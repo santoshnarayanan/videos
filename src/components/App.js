462f2f8343c68f4d874025f52a2fcfb69c73fe8a
santosh33 <santosh33@gmail.com>
2022-06-22 22:15:28 +0530

    displaying  video frm youtube and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,43 @@
 import SearchBar from "./searchBar";
 import youtube from "./apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import React from "react";
 
-class App extends React.Component{
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
 
-    state = {videos: [] };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    this.setState({ videos: response.data.items });
+  };
 
-    onTermSubmit = async (term) =>{
-        const response = await youtube.get('/search', {
-            params:{
-                q:term
-            }
-        });
-        this.setState({videos: response.data.items});
-    };
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
 
-    render(){
-        return (
-            <div className="ui container">
-                <SearchBar onFormSubmit ={this.onTermSubmit} />
-                I have {this.state.videos.length} videos.
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className=" eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
             </div>
-        );
-    }
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/VideoList.js
@@ -0,0 +1,16 @@
+import VideoItem from "./VideoItem";
+import React from "react";
+
+const VideoList = ({videos, onVideoSelect})=> {
+    const renderedList = videos.map((video) => {
+        return <VideoItem key={video.id.videoId}
+        onVideoSelect={onVideoSelect} video={video}/>;
+    });
+    return (
+        <div className="ui relaxed divided list">
+            {renderedList}
+        </div>
+    );
+};
+
+export default VideoList;
